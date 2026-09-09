9c61b6165ee6f188aa8eb181b028a9d5e7bf0afb
Alexey Yuzhakov <sibprogrammer@gmail.com>
2026-09-09 19:53:06 +0100

    Fix window dimensions after refactoring

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pgzrun
 
 from kinetix.application import draw, initialize, on_key_down, update
+from kinetix.constants import HEIGHT, WIDTH
 
-__all__ = ["draw", "on_key_down", "update"]
+__all__ = ["HEIGHT", "WIDTH", "draw", "on_key_down", "update"]
 
 
 initialize()
