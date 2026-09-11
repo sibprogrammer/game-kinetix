86d10c378ad3341ff06684530abd157afa6e0f02
Alexey Yuzhakov <sibprogrammer@gmail.com>
2026-09-11 22:40:12 +0100

    Fix window title missed after refactoring

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pgzrun
 
 from kinetix.application import draw, initialize, on_key_down, update
-from kinetix.constants import HEIGHT, WIDTH
+from kinetix.constants import HEIGHT, WIDTH, TITLE
 
-__all__ = ["HEIGHT", "WIDTH", "draw", "on_key_down", "update"]
+__all__ = ["HEIGHT", "WIDTH", "TITLE", "draw", "on_key_down", "update"]
 
 
 initialize()
